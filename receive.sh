b867062b48cd1aba0e78654bf5a447c8e3a22c02
Adriano Ferrari <adriano.ferrari@gmail.com>
2026-04-23 11:19:13 -0400

    print current IP address in receive.sh

--- receive.sh
+++ receive.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PORT=5000
-echo "Listening on port $PORT... Ctrl+C to quit."
+IP=$(hostname -I | awk '{print $1}')
+echo "IP Address for this device: $IP. Listening on port $PORT... Ctrl+C to quit."
 echo ""
 
 while true; do
