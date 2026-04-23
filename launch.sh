a790c6355b2a530d807db218aaed3d07ab08afe9
Adriano Ferrari <adriano.ferrari@gmail.com>
2026-04-23 14:06:38 -0400

    modify launch.sh to be background/disowned

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-google-chrome --app="file://$DIR/index.html"
+google-chrome --app="file://$DIR/index.html" &>/dev/null &
+disown
